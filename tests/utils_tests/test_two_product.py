16a05739c8a99231e92a038fc5ac8a2c605853ec
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-30 04:14:11 +0300

    Add test for 'two_product' function

---
+++ tests/utils_tests/test_two_product.py
@@ -0,0 +1,35 @@
+from typing import Tuple
+
+from hypothesis import given
+
+from robust.hints import Scalar
+from robust.utils import two_product
+from tests import strategies
+
+
+@given(strategies.scalars_pairs)
+def test_basic(scalars_pair: Tuple[Scalar, Scalar]) -> None:
+    left, right = scalars_pair
+
+    result = two_product(left, right)
+
+    assert isinstance(result, tuple)
+    assert len(result) == 2
+    assert all(isinstance(element, type(left)) for element in result)
+
+
+@given(strategies.scalars_pairs)
+def test_commutativity(scalars_pair: Tuple[Scalar, Scalar]) -> None:
+    left, right = scalars_pair
+
+    assert two_product(left, right) == two_product(right, left)
+
+
+@given(strategies.scalars)
+def test_left_absorbing_element(scalar: Scalar) -> None:
+    assert two_product(0, scalar) == (0, 0)
+
+
+@given(strategies.scalars)
+def test_right_absorbing_element(scalar: Scalar) -> None:
+    assert two_product(scalar, 0) == (0, 0)
