a7e4b6ab6f979f7036d959df80bd12084019e595
Azat Ibrakov <azatibrakov@gmail.com>
2020-01-08 23:22:44 +0300

    Complete test for 'two_diff' function

--- tests/utils_tests/test_two_diff.py
+++ tests/utils_tests/test_two_diff.py
@@ -18,6 +18,15 @@ def test_basic(scalars_pair: Tuple[Scalar, Scalar]) -> None:
     assert all(isinstance(element, type(left)) for element in result)
 
 
+@given(strategies.scalars_pairs)
+def test_properties(scalars_pair: Tuple[Scalar, Scalar]) -> None:
+    left, right = scalars_pair
+
+    approximation, tail = two_diff(left, right)
+
+    assert approximation + tail == left - right
+
+
 @given(strategies.scalars)
 def test_right_neutral_element(scalar: Scalar) -> None:
     assert two_diff(scalar, 0) == (scalar, 0)
