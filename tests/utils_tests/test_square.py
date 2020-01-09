0f743d95233350340b496e90a52762c6ed0b5df6
Azat Ibrakov <azatibrakov@gmail.com>
2020-01-09 19:59:58 +0300

    Add test for 'square' function

---
+++ tests/utils_tests/test_square.py
@@ -0,0 +1,24 @@
+from hypothesis import given
+
+from robust.hints import Scalar
+from robust.utils import square
+from tests import strategies
+from tests.utils import are_non_overlapping_numbers
+
+
+@given(strategies.scalars)
+def test_basic(scalar: Scalar) -> None:
+    result = square(scalar)
+
+    assert isinstance(result, tuple)
+    assert len(result) == 2
+    assert all(isinstance(element, type(scalar)) for element in result)
+
+
+@given(strategies.scalars)
+def test_properties(scalar: Scalar) -> None:
+    estimation, tail = square(scalar)
+
+    assert estimation + tail == scalar ** 2
+    assert abs(estimation) >= abs(tail)
+    assert are_non_overlapping_numbers(estimation, tail)
