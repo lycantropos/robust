b161734ff41091486511e1032b3ed7efeff393e6
Azat Ibrakov <azatibrakov@gmail.com>
2020-01-09 19:32:21 +0300

    Complete test for 'fast_two_sum' function

--- tests/utils_tests/test_fast_two_sum.py
+++ tests/utils_tests/test_fast_two_sum.py
@@ -26,6 +26,7 @@ def test_properties(scalars_pair: Tuple[Scalar, Scalar]) -> None:
     approximation, tail = fast_two_sum(left, right)
 
     assert approximation + tail == left + right
+    assert abs(approximation) >= abs(tail)
     assert are_non_overlapping_numbers(approximation, tail)
 
 
