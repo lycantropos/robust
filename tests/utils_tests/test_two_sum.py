4f328a013bc2e32ae4ca1d8e0b483bf4daeaca22
Azat Ibrakov <azatibrakov@gmail.com>
2020-01-13 14:47:50 +0300

    Complete test for 'two_sum' function

--- tests/utils_tests/test_two_sum.py
+++ tests/utils_tests/test_two_sum.py
@@ -3,8 +3,7 @@ from typing import Tuple
 from hypothesis import given
 
 from robust.hints import Scalar
-from robust.utils import (fast_two_sum,
-                          two_sum)
+from robust.utils import two_sum
 from tests import strategies
 from tests.utils import (is_non_overlapping_expansion,
                          is_sorted_by_magnitude_expansion)
@@ -30,11 +29,3 @@ def test_properties(scalars_pair: Tuple[Scalar, Scalar]) -> None:
     assert sum(result) == left + right
     assert is_sorted_by_magnitude_expansion(result)
     assert is_non_overlapping_expansion(result)
-
-
-@given(strategies.reverse_sorted_by_modulus_scalars_pairs)
-def test_connection_with_fast_two_sum(scalars_pair: Tuple[Scalar, Scalar]
-                                      ) -> None:
-    left, right = scalars_pair
-
-    assert two_sum(left, right) == fast_two_sum(left, right)
