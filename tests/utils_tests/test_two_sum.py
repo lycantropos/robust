141586bb97118d36f1a614751712a17d12e0de30
Azat Ibrakov <azatibrakov@gmail.com>
2020-01-13 14:10:56 +0300

    Complete test for 'two_sum' function

--- tests/utils_tests/test_two_sum.py
+++ tests/utils_tests/test_two_sum.py
@@ -32,16 +32,6 @@ def test_properties(scalars_pair: Tuple[Scalar, Scalar]) -> None:
     assert is_non_overlapping_expansion(result)
 
 
-@given(strategies.scalars)
-def test_left_neutral_element(scalar: Scalar) -> None:
-    assert two_sum(0, scalar) == (0, scalar)
-
-
-@given(strategies.scalars)
-def test_right_neutral_element(scalar: Scalar) -> None:
-    assert two_sum(scalar, 0) == (0, scalar)
-
-
 @given(strategies.reverse_sorted_by_modulus_scalars_pairs)
 def test_connection_with_fast_two_sum(scalars_pair: Tuple[Scalar, Scalar]
                                       ) -> None:
