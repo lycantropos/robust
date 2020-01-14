5c603aa99a565851679258a745ac7b956a768059
Azat Ibrakov <azatibrakov@gmail.com>
2020-01-14 20:12:10 +0300

    Complete docstring for 'projection.signed_length' function

--- robust/projection.py
+++ robust/projection.py
@@ -9,6 +9,10 @@ def signed_length(first_start: Point, first_end: Point,
                   second_start: Point, second_end: Point) -> Real:
     """
     Calculates signed length of projection of one vector onto another.
+
+    Positive sign of result means that angle between vectors is acute,
+    negative -- obtuse,
+    zero -- right.
     """
     return parallelogram.signed_area(first_start, first_end,
                                      to_perpendicular_point(second_start),
