7b6d5ce21a56983b3265344c650f8fe5aece7cdf
Azat Ibrakov <azatibrakov@gmail.com>
2020-12-03 22:10:44 +0300

    Complete formatting

--- robust/projection.py
+++ robust/projection.py
@@ -5,8 +5,10 @@ from .hints import Point
 from .utils import to_perpendicular_point
 
 
-def signed_length(first_start: Point, first_end: Point,
-                  second_start: Point, second_end: Point) -> Real:
+def signed_length(first_start: Point,
+                  first_end: Point,
+                  second_start: Point,
+                  second_end: Point) -> Real:
     """
     Calculates signed length of projection of one vector onto another.
 
