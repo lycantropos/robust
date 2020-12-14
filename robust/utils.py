1ad8ff05f893cdea562321530fe642c9db883363
Azat Ibrakov <azatibrakov@gmail.com>
2020-12-14 17:54:00 +0300

    Complete utils

--- robust/utils.py
+++ robust/utils.py
@@ -7,20 +7,20 @@ from .hints import (Expansion,
 
 
 def fast_two_sum(left: Real, right: Real) -> Tuple[Real, Real]:
-    estimation = left + right
-    right_virtual = estimation - left
+    head = left + right
+    right_virtual = head - left
     tail = right - right_virtual
-    return tail, estimation
+    return tail, head
 
 
 def two_sum(left: Real, right: Real) -> Tuple[Real, Real]:
-    estimation = left + right
-    right_virtual = estimation - left
-    left_virtual = estimation - right_virtual
+    head = left + right
+    right_virtual = head - left
+    left_virtual = head - right_virtual
     right_tail = right - right_virtual
     left_tail = left - left_virtual
     tail = left_tail + right_tail
-    return tail, estimation
+    return tail, head
 
 
 def split(value: Real,
@@ -32,80 +32,87 @@ def split(value: Real,
     return low, high
 
 
-def two_product_presplit(left: Real, right: Real, right_low: Real,
+def two_product_presplit(left: Real,
+                         right: Real,
+                         right_low: Real,
                          right_high: Real) -> Tuple[Real, Real]:
-    estimation = left * right
+    head = left * right
     left_low, left_high = split(left)
-    first_error = estimation - left_high * right_high
+    first_error = head - left_high * right_high
     second_error = first_error - left_low * right_high
     third_error = second_error - left_high * right_low
     tail = left_low * right_low - third_error
-    return tail, estimation
+    return tail, head
 
 
 def two_product(left: Real, right: Real) -> Tuple[Real, Real]:
-    estimation = left * right
+    head = left * right
     left_low, left_high = split(left)
     right_low, right_high = split(right)
-    first_error = estimation - left_high * right_high
+    first_error = head - left_high * right_high
     second_error = first_error - left_low * right_high
     third_error = second_error - left_high * right_low
     tail = left_low * right_low - third_error
-    return tail, estimation
+    return tail, head
 
 
-def two_two_diff(left_tail: Real, left: Real, right_tail: Real,
-                 right: Real) -> Tuple[Real, Real, Real, Real]:
-    third_tail, interim_tail, interim = two_one_diff(left_tail, left,
-                                                     right_tail)
-    second_tail, first_tail, estimation = two_one_diff(interim_tail, interim,
-                                                       right)
-    return third_tail, second_tail, first_tail, estimation
+def two_two_diff(left_tail: Real,
+                 left_head: Real,
+                 right_tail: Real,
+                 right_head: Real) -> Tuple[Real, Real, Real, Real]:
+    third_tail, mid_tail, mid_head = two_one_diff(left_tail, left_head,
+                                                  right_tail)
+    second_tail, first_tail, head = two_one_diff(mid_tail, mid_head,
+                                                 right_head)
+    return third_tail, second_tail, first_tail, head
 
 
-def two_two_sum(left_tail: Real, left: Real, right_tail: Real,
-                right: Real) -> Tuple[Real, Real, Real, Real]:
-    third_tail, interim_tail, interim = two_one_sum(left_tail, left,
-                                                    right_tail)
-    second_tail, first_tail, estimation = two_one_sum(interim_tail, interim,
-                                                      right)
-    return third_tail, second_tail, first_tail, estimation
+def two_two_sum(left_tail: Real,
+                left_head: Real,
+                right_tail: Real,
+                right_head: Real) -> Tuple[Real, Real, Real, Real]:
+    third_tail, mid_tail, mid_head = two_one_sum(left_tail, left_head,
+                                                 right_tail)
+    second_tail, first_tail, head = two_one_sum(mid_tail, mid_head, right_head)
+    return third_tail, second_tail, first_tail, head
 
 
-def two_one_sum(left_tail: Real, left: Real,
+def two_one_sum(left_tail: Real,
+                left_head: Real,
                 right: Real) -> Tuple[Real, Real, Real]:
-    second_tail, interim = two_sum(left_tail, right)
-    first_tail, estimation = two_sum(left, interim)
-    return second_tail, first_tail, estimation
+    second_tail, mid_head = two_sum(left_tail, right)
+    first_tail, head = two_sum(left_head, mid_head)
+    return second_tail, first_tail, head
 
 
-def two_one_diff(left_tail: Real, left: Real,
+def two_one_diff(left_tail: Real,
+                 left_head: Real,
                  right: Real) -> Tuple[Real, Real, Real]:
-    second_tail, interim = two_diff(left_tail, right)
-    first_tail, estimation = two_sum(left, interim)
-    return second_tail, first_tail, estimation
+    second_tail, mid_head = two_diff(left_tail, right)
+    first_tail, head = two_sum(left_head, mid_head)
+    return second_tail, first_tail, head
 
 
 def two_diff(left: Real, right: Real) -> Tuple[Real, Real]:
-    estimation = left - right
-    return two_diff_tail(left, right, estimation), estimation
+    head = left - right
+    return two_diff_tail(left, right, head), head
 
 
-def two_diff_tail(left: Real, right: Real, estimation: Real) -> Real:
-    right_virtual = left - estimation
-    left_virtual = estimation + right_virtual
+def two_diff_tail(left: Real, right: Real, head: Real) -> Real:
+    right_virtual = left - head
+    left_virtual = head + right_virtual
     right_error = right_virtual - right
     left_error = left - left_virtual
     return left_error + right_error
 
 
 def square(value: Real) -> Tuple[Real, Real]:
-    estimation = value * value
+    head = value * value
     value_low, value_high = split(value)
-    first_error = estimation - value_high * value_high
+    first_error = head - value_high * value_high
     second_error = first_error - (value_high + value_high) * value_low
     tail = value_low * value_low - second_error
-    return tail, estimation
+    return tail, head
 
 
 def sum_expansions(left_expansion: Expansion,
@@ -193,11 +200,12 @@ def to_cross_product(minuend_multiplier_x: Real,
     """
     Returns expansion of vectors' planar cross product.
     """
-    minuend_tail, minuend = two_product(minuend_multiplier_x,
-                                        minuend_multiplier_y)
-    subtrahend_tail, subtrahend = two_product(subtrahend_multiplier_y,
-                                              subtrahend_multiplier_x)
-    return two_two_diff(minuend_tail, minuend, subtrahend_tail, subtrahend)
+    minuend_tail, minuend_head = two_product(minuend_multiplier_x,
+                                             minuend_multiplier_y)
+    subtrahend_tail, subtrahend_head = two_product(subtrahend_multiplier_y,
+                                                   subtrahend_multiplier_x)
+    return two_two_diff(minuend_tail, minuend_head, subtrahend_tail,
+                        subtrahend_head)
 
 
 def to_perpendicular_point(point: Point) -> Point:
